85b16aedb666cf4cb2d9cf62ac7550c8ac4cc95f
asaf shahar <asaf60@gmail.com>
2014-09-03 22:34:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/teamb/israelgrid/GPSItmActivity.java
@@ -0,0 +1,222 @@
+package com.example.teamb.israelgrid;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class GPSItmActivity extends Activity {
+
+    private TextView mTextViewLatitude;
+    private TextView mTextViewLongitude;
+    private TextView mTextViewSpeed;
+    private TextView mTextViewITM;
+    private TextView mTextViewAccuracy;
+    private ImageView mCompassImage;
+
+    static final int MIN_UPDATE_TIME = 500;
+    static final int MIN_UPDATE_DISTANCE = 0;
+    static final String TAG="GEOITM";
+    private float mDegrees;
+    private LocationManager mLocationManager;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagneticField;
+    private boolean hasCompass = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goeitm);
+
+        mTextViewLatitude = (TextView)findViewById(R.id.TextView_Latitude);
+        mTextViewLongitude = (TextView)findViewById(R.id.TextView_Longitude);
+        mTextViewSpeed = (TextView)findViewById(R.id.TextView_Speed);
+        mTextViewITM = (TextView)findViewById(R.id.TextView_ITM);
+        mTextViewAccuracy = (TextView)findViewById(R.id.TextView_Accuracy);
+        mCompassImage = (ImageView)findViewById(R.id.ImageView_Compass);
+
+        mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (mMagneticField!=null && mAccelerometer!=null) {
+            hasCompass = true;
+        } else {
+            hasCompass = false;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.goeitm, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.removeUpdates(mLocationListener);
+        if (hasCompass) {
+            mSensorManager.unregisterListener(mSensorEventListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                MIN_UPDATE_TIME ,
+                MIN_UPDATE_DISTANCE,
+                mLocationListener);
+        if (hasCompass) {
+            mSensorManager.registerListener(mSensorEventListener, mMagneticField, SensorManager.SENSOR_DELAY_UI);
+            mSensorManager.registerListener(mSensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        }
+        //resetFields();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void resetFields() {
+        mTextViewLatitude.setText(R.string.location_unavailable);
+        mTextViewLongitude.setText(R.string.location_unavailable);
+        mTextViewSpeed.setText(R.string.location_unavailable);
+        mTextViewITM.setText(R.string.location_unavailable);
+        mTextViewAccuracy.setText(R.string.location_unavailable);
+        rotateImage(0);
+    }
+
+    private void updateLocation(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        mTextViewLatitude.setText(Location.convert(lat, Location.FORMAT_SECONDS));
+        mTextViewLongitude.setText(Location.convert(lng, Location.FORMAT_SECONDS));
+
+        long[] itm = new long[2];
+        GeoUtils.GeoToITM(location.getLatitude(), location.getLongitude(), itm);
+        mTextViewITM.setText(itm[1] + "N " + itm[0]+ "E");
+
+        if (location.hasAccuracy()) {
+            mTextViewAccuracy.setText(String.format("%.2fm", location.getAccuracy()));
+        } else {
+            mTextViewAccuracy.setText(R.string.location_unavailable);
+        }
+
+        String speed = getResources().getText(R.string.location_unavailable).toString();
+        if (location.hasSpeed()) {
+            speed = String.format("%dkm/h",Math.round(location.getSpeed()*3.6));
+        }
+
+        if (location.hasBearing()) {
+            if (!hasCompass) {
+                rotateImage(location.getBearing());
+            }
+        }
+        mTextViewSpeed.setText(speed);
+    }
+
+    private void rotateImage(float degrees) {
+        RotateAnimation ra = new RotateAnimation(-mDegrees, -degrees,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+        mCompassImage.startAnimation(ra);
+        mDegrees = degrees;
+    }
+
+    private LocationListener mLocationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            updateLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+
+        private float[] mGravity = null;
+        private float[] mGeomagnetic = null;
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                mGravity = sensorEvent.values;
+            }
+
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                mGeomagnetic = sensorEvent.values;
+            }
+
+            if (mGravity != null && mGeomagnetic != null) {
+                float R[] = new float[9];
+                float I[] = new float[9];
+                boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+                if (success) {
+                    float orientation[] = new float[3];
+                    // orientation contains: azimuth, pitch and roll
+                    SensorManager.getOrientation(R, orientation);
+                    rotateImage((float) Math.toDegrees(orientation[0]));
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/teamb/israelgrid/GeoUtils.java
@@ -0,0 +1,67 @@
+package com.example.teamb.israelgrid;
+
+/**
+ * Created by asafsh on 03/09/14.
+ */
+public class GeoUtils {
+    /**
+     * Convert gps coordinates to Israel new geographic system
+     * @param lat location latitude in degrees
+     * @param lng location longitude in degrees
+     * @param out out an array of long put East in out[0] and
+     *            North in out[1]
+     */
+    public static void GeoToITM (double lat, double lng, long[] out) {
+        // scale factor
+        double k0 = 1.0000067;
+
+        // central meridian of ITM projection
+        double P = Math.toRadians(lng)-Math.toRadians(35.2045169444444);
+
+        // Ellipsoid constants WGS 80 datum
+        double a = 6378137;  // equatorial radius
+        double b = 6356752.3141; // polar radius
+        double esq = (1 - (b/a)*(b/a));
+        double e = Math.sqrt(esq);  // eccentricity
+        double e1sq = e*e/(1-e*e);
+        //double n = (a-b)/(a+b);
+
+        double phi = Math.toRadians(lat);
+
+        // Curvature at specified location
+        double nu = a/Math.sqrt(1-Math.pow(e*Math.sin(phi),2));
+
+        // Arc length along standard meridian
+        double M =  phi*(1 - esq*(1.0/4.0 + esq*(3.0/64.0 + 5.0*esq/256.0)));
+        M -= Math.sin(2*phi)*(esq*(3.0/8.0 + esq*(3.0/32.0 + 45.0*esq/1024.0)));
+        M += Math.sin(4*phi)*(esq*esq*(15.0/256.0 + esq*45.0/1024.0));
+        M -= Math.sin(6*phi)*(esq*esq*esq*(35.0/3072.0));
+        M  = M*a;
+
+        double T0 = Math.sin(phi);
+        double T1 = Math.cos(phi);
+        double T2 = T1*T1;
+        double T3 = Math.pow(Math.tan(phi),2);
+
+        double K1 = k0*M;
+        double K2 = k0*nu*T0*T1/2;
+        double K3 = k0*((nu*T0*T1*T2)/24)*(5-T3+9*e1sq*T2+4*e1sq*e1sq*T2*T2);
+        double K4 = k0*nu*T1;
+        double K5 = k0*T1*T2*(nu/6)*(1-T3+e1sq*T2);
+
+        // Easting relative to central meridian
+        out[0] = Math.round(K4*P+K5*P*P*P + 219529.58);
+        // Northing from equator
+        out[1] = Math.round(K1 + K2*P*P + K3*P*P*P*P - 3512424.41 + 626907.39);
+    }
+
+    public static double distance(double lat1, double lng1, double lat2, double lng2) {
+        double l1 = Math.cos(Math.toRadians(lat1));
+        double l2 = Math.cos(Math.toRadians(lat2));
+        double d0 = Math.sin(Math.toRadians(lat2-lat1)/2);
+        double d1 = Math.sin(Math.toRadians(lng2-lng1)/2);
+        double a = d0*d0+l1*l2*d1*d1;
+        double d = 2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        return d*6378137;
+    }
+}
